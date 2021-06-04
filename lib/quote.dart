b3de3e5bf77aeddf34104b4578944a3a9375dba2
crpoudyal <crpoudyal@gmail.com>
2021-06-04 09:54:37 +0545

    UI changes

--- lib/quote.dart
+++ lib/quote.dart
@@ -15,29 +15,45 @@ class _QuoteState extends State<Quote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orangeAccent,
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              quote[index % quote.length],
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            TextButton(
-              onPressed: ShowQuotes,
-              child: Text("Next"),
-            ),
-          ],
+      backgroundColor: Colors.deepOrangeAccent,
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 400,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                quote[index % quote.length],
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: showQuotes,
+                child: Text("Next"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void ShowQuotes() {
+  void showQuotes() {
     setState(() {
       index++;
     });
